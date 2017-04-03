a15d4bcc6005c6b102d4722d7c97e97ed0223555
RoanWC <RoanWC@ROANSLAPTOP>
2017-04-03 08:25:02 -0400

    Completed creating author objects from the xml document. Created anonymous book objects from the sml and turn them into Book objects. use the anonymous book objects to find out which author to add the book objects to. There is an error in the db.authors.where ("sequence contains no elements")

--- loadBookClub/Program.cs
+++ loadBookClub/Program.cs
@@ -11,13 +11,23 @@ namespace loadBookClub
     {
         static void Main(string[] args)
         {
-            XElement authornames = XElement.Load("books.xml");
-           // var authorFNames = from fname in authornames.Descendants("author") select fname.Attribute("firstName");
-           // var authorLNames = from lname in authornames.Descendants("author") select lname;//.Attribute("lastName");
-            var authorNames = from a in authornames.Descendants("author") select a;
-            using(var db = new BookClubDB())
+            XElement booksXML = XElement.Load("books.xml");
+            // var authorFNames = from fname in authornames.Descendants("author") select fname.Attribute("firstName");
+            // var authorLNames = from lname in authornames.Descendants("author") select lname;//.Attribute("lastName");
+            
+            
+
+
+            using (var db = new BookClubDB())
             {
-                foreach(var i in authorNames)
+                
+
+                //create all of the author objects
+                var authorNames = from a in booksXML.Descendants("author") select a;
+                List<String> insertedNames = new List<string>();
+                var aNames = from a in authorNames select a;
+                
+                foreach (var i in aNames)
                 {
                     if (i.Attribute("firstName") != null)
                     {
@@ -26,37 +36,81 @@ namespace loadBookClub
                             FIRSTNAME = i.Attribute("firstName").Value,
                             LASTNAME = i.Attribute("lastName").Value
                         };
-                        int[] authAttributes = { a.AUTHOR_ID};
-                        if (db.authors.Find(a.AUTHOR_ID) == null)
+                        String authorname = a.FIRSTNAME.ToString() + a.LASTNAME.ToString();
+                        if (!(insertedNames.Contains(authorname)))
                         {
                             db.authors.Add(a);
+                            insertedNames.Add(authorname);
                         }
-
-                    } 
-                    if(i.Attribute("firstName") == null)
+                    }
+                    if (i.Attribute("firstName") == null)
                     {
                         author a = new author
                         {
                             FIRSTNAME = "not available",
                             LASTNAME = i.Attribute("lastName").Value
                         };
-                        int[] authAttributes = { a.AUTHOR_ID};
-                        if (db.authors.Find(a.AUTHOR_ID) == null)
+                        String authorname = a.FIRSTNAME.ToString() + a.LASTNAME.ToString();
+                        if (!(insertedNames.Contains(authorname)))
                         {
                             db.authors.Add(a);
+                            insertedNames.Add(authorname);
                         }
+                    }          
+                }//done creating authors
+                                              
+                //create anonymous objects for the books
+                var XMLbooks = from b in booksXML.Descendants("book") select b;
+                List<Object> anonymousBooks = new List<Object>();
+                List<book> allBooks = new List<book>();
+                foreach(var b in XMLbooks)
+                {
+                    var anonBook = new
+                    {
+                        TITLE = b.Element("title").Value,
+                        DESCRIPTION = b.Element("description").Value,
+                        authorFName = b.Element("author").Attribute("firstName")?.Value ,
+                        authorLName = b.Element("author").Attribute("lastName").Value
+                    };
+                    anonymousBooks.Add(anonBook);
+                    book book = new book
+                    {
+                        TITLE = anonBook.TITLE,
+                        DESCRIPTION = anonBook.DESCRIPTION
+                    };
+                    allBooks.Add(book);
+                    db.books.Add(book);
 
-                    }
+
+
+
+
+                    author auth = db.authors.Where(a => a.FIRSTNAME == anonBook.authorFName && a.LASTNAME == anonBook.authorLName).First();
+                    auth.books.Add(book);
+                   /* var authors = from a in db.authors select a;
+                     var anAuthor = (from a in authors
+                                       where a.FIRSTNAME == anonBook.authorFName && a.LASTNAME == anonBook.authorLName
+                                       select a);
+                   // anAuthor.books.Add(book);*/
                 }
-                db.SaveChanges();
+
+                Console.WriteLine(anonymousBooks);
+
+                //db.SaveChanges();
             }
+            /*
             foreach(var i in authorNames)
             {
+                if (i.Attribute("firstName") != null)
+                    Console.Write(i.Attribute("firstName").Value + " ");
+                else
+                    Console.Write("N/A");
                 Console.WriteLine(i.Attribute("lastName").Value);
                 
             }
-
+            */
             //Console.Write(authorFNames);
+            Console.WriteLine("Execution completed");
             Console.Read();
         }
     }
