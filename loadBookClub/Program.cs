46b7ac9f989adf627e2a5c40b4e8ca277c7600ef
RoanWC <RoanWC@ROANSLAPTOP>
2017-04-08 12:52:36 -0400

    Completed phase two. Database is now filled with the data from the xml file. moving on to the next phase.

--- loadBookClub/Program.cs
+++ loadBookClub/Program.cs
@@ -14,14 +14,7 @@ namespace loadBookClub
     {
         static void Main(string[] args)
         {
-
             XElement booksXML = XElement.Load(@"Files/books.xml");
-            // var authorFNames = from fname in authornames.Descendants("author") select fname.Attribute("firstName");
-            // var authorLNames = from lname in authornames.Descendants("author") select lname;//.Attribute("lastName");
-
-
-
-
             //create author objects and put them in a list of authors.
             var authorNames = from a in booksXML.Descendants("author") select a;
             List<String> insertedNames = new List<string>();
@@ -62,14 +55,12 @@ namespace loadBookClub
                 }
             }//done creating authors
 
-
             //create anonymous objects for the books and use them to create books and add them to the correct author before putting the books in a list
             var XMLbooks = from b in booksXML.Descendants("book") select b;
             List<Object> anonymousBooks = new List<Object>();
             List<book> allBooks = new List<book>();
             foreach (var b in XMLbooks)
             {
-
                 var anonBook = new
                 {
                     TITLE = b.Element("title").Value,
@@ -85,13 +76,10 @@ namespace loadBookClub
                 };
                 allBooks.Add(book);
 
-                var auth = authorsList.Where(a => a.FIRSTNAME == anonBook.authorFName && a.LASTNAME == anonBook.authorLName).FirstOrDefault();
-
-                //Console.WriteLine("alert");
+                var auth = authorsList.Where(a => a.FIRSTNAME == anonBook.authorFName
+                                                && a.LASTNAME == anonBook.authorLName).FirstOrDefault();
                 auth?.books.Add(book);
-
-                //Console.WriteLine(auth?.FIRSTNAME + " " + auth.LASTNAME + " wrote " + auth.books?.First().TITLE);
-            }//end foreach 
+            }//end foreach
 
             XElement ratingsXML = XElement.Load(@"Files/ratings.xml");
             List<user> userList = new List<user>();
@@ -119,37 +107,27 @@ namespace loadBookClub
                         RATING = int.Parse(r.Attribute("rating").Value),
                         CONTENT = "",
                         USERNAME = newUser.USERNAME
-                        
                     };
                     newUser.reviews.Add(newReview);
                     reviewList.Add(newReview);
-                }     
+                }
             }
             Console.WriteLine("alert");
-            
-            //put the books and authors into the db set and save changes to the database
+
+            //fill up the dbContext and save the changes to the database
             using (var db = new BookClubDB())
             {
-
-                 //i do not need to add the books to the dbset because they are all in author objects.                             
+                //only authors and users need to be added to the dbcontext because they contain lists on books and reviews.
                 foreach (author a in authorsList)
                 {
                     db.authors.Add(a);
                 }
-                
                 foreach(user u in userList)
                 {
                     db.users.Add(u);
                 }
-
-                foreach(review r in reviewList)
-                {
-                    db.reviews.Add(r);
-                }
-                //Console.Write("alert");
                db.SaveChanges();
             }
-            
             Console.WriteLine("Execution completed");
             Console.Read();
         }
