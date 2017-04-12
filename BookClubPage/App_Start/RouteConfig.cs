21dc861a170740aa45f0d46f164608ca0ed0529d
1542745 <1542745@2F16-26>
2017-04-12 16:42:34 -0400

    Application Starts

--- BookClubPage/App_Start/RouteConfig.cs
+++ BookClubPage/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BookClubPage
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "users", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
